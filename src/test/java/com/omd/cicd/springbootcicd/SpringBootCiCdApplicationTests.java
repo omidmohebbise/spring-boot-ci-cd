c8eed344f476b48574b652756eda9cc633f8592a
omid mohebbi <omidmohebbise@gmail.com>
2022-04-12 18:09:54 +0430

    Craete a Simple App

---
+++ src/test/java/com/omd/cicd/springbootcicd/SpringBootCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omd.cicd.springbootcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
